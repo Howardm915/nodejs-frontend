67fd1f3c2a8b61b11382fa89f09150635d5447ed
howardm915 <howardmais@yahoo.com>
2026-05-08 17:54:36 -0400

    Fix env-config.js route filename to match HTML

--- index.js
+++ index.js
@@ -10,7 +10,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.get('/env-configs.js', (req, res) => {
+app.get('/env-config.js', (req, res) => {
+        res.type('application/javascript');
+        res.send(
+            `window.env = ${JSON.stringify({
+                COMPONENT_BACKEND_URL: process.env.COMPONENT_BACKEND_URL || '',
+                // add whatever other vars the React app reads from window.env
+            })};`
+        );
+    });
+
+
+app.get('/env-config.js', (req, res) => {
         res.type('application/javascript');
         res.send(
             `window.env = ${JSON.stringify({
